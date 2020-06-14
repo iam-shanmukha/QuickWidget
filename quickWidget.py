3d93978fd2dcc6ac79d0302a9d3fa42cf7ab4bb9
iam-shanmukha <vishnushanmukha@gmail.com>
2020-06-14 21:42:41 +0530

    modified:   quickWidget.py 		:   using Frame

--- quickWidget.py
+++ quickWidget.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
-import tkinter as tk
+from tkinter import *
 import os
-root=tk.Tk()
-frame = tk.Frame(root)
+root=Tk()
+root.geometry("200x500")
+frame = Frame(root)
 frame.pack()
+
+
+leftframe = Frame(root,bd = 9, bg = "blue", cursor = "arrow")
+leftframe.pack(side=TOP)
+
+label = Label(frame, text = "A script by Shanmukha Vishnu(iam-shanmukha)")
+label.pack()
 
 tasks={
 	"open firefox":"firefox&",
@@ -14,7 +22,7 @@ tasks={
 	"Open Coursera":'firefox coursera.org&',
 	"open INFYTQ":'firefox https://infytq.infosys.com/home&',
 	"Open Gmail":'firefox gmail.com&', 
-	"Open Twitter":'firefox twitter.com&', 
+	"Open twitter":'firefox twitter.com&', 
 	"Read Latest News":'firefox eenadu.net&', 
 	"Open Sublime":'/opt/sublime_text/sublime_text&',
 	"Kill Firefox":"killall -9 firefox&",
@@ -24,9 +32,12 @@ tasks={
 #commands=['firefox', 'thunar $HOME','sensors','firefox coursera.org&','firefox gmail.com&','firefox twitter.com', 'firefox eenadu.net','/opt/sublime_text/sublime_text','quit']
 
 for task,cmd in tasks.items():
-	slogan = tk.Button(frame,text=task,command=lambda cmd=cmd: os.system(cmd))
-	#slogan.pack(side=tk.TOP)
-	slogan.grid(columnspan=4)
+	buttons = Button(leftframe, text = task, command=lambda cmd=cmd: os.system(cmd))
+	buttons.pack(padx =10, pady=5)
+	#slogan = tk.Button(frame,text=task,command=lambda cmd=cmd: os.system(cmd))
+	#slogan.pack(side="right")
+	#slogan.pack(expand=1)
+	#slogan.grid(columnspan=4)
 	#print(task,cmd)
 print(os.system("figlet -f slant QuickWidget"))
 print("Made with love by shanmukha vishnu")
