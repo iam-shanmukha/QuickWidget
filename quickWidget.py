1df899a71a257aea2e59fa7963b1558f9a399ecb
iam-shanmukha <vishnushanmukha@gmail.com>
2020-06-14 20:43:40 +0530

    modified:   quickWidget.py 		    Added More Commands

--- quickWidget.py
+++ quickWidget.py
@@ -7,14 +7,19 @@ frame.pack()
 
 tasks={
 	"open firefox":"firefox&",
-	"Open File Manager":'thunar $HOME&',
+	"open Terminal":"exo-open --launch TerminalEmulator",
+	"PDF Viewer":"qpdfview --unique&",
+	"Open File Manager":"thunar $HOME&",
 	"Show sensors":'xfce4-sensors&',
 	"Open Coursera":'firefox coursera.org&',
+	"open INFYTQ":'firefox https://infytq.infosys.com/home&',
 	"Open Gmail":'firefox gmail.com&', 
 	"Open Twitter":'firefox twitter.com&', 
 	"Read Latest News":'firefox eenadu.net&', 
 	"Open Sublime":'/opt/sublime_text/sublime_text&',
-	"Kill Firefox":"killall -9 firefox&"
+	"Kill Firefox":"killall -9 firefox&",
+	"Music":"audacious",
+	"Reboot":"reboot"
 	}
 #commands=['firefox', 'thunar $HOME','sensors','firefox coursera.org&','firefox gmail.com&','firefox twitter.com', 'firefox eenadu.net','/opt/sublime_text/sublime_text','quit']
 
@@ -22,7 +27,9 @@ for task,cmd in tasks.items():
 	slogan = tk.Button(frame,text=task,command=lambda cmd=cmd: os.system(cmd))
 	#slogan.pack(side=tk.TOP)
 	slogan.grid(columnspan=4)
-	print(task,cmd)
+	#print(task,cmd)
+print(os.system("figlet -f slant QuickWidget"))
+print("Made with love by shanmukha vishnu")
 root.mainloop()
 
 #https://stackoverflow.com/questions/10865116/tkinter-creating-buttons-in-for-loop-passing-command-arguments
